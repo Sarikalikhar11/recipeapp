5c21fba7e34166fb04c6ff0a4a123408584dc276
Sarikalikhar11 <sarikalikhar2619@gmail.com>
2022-07-20 16:33:01 +0530

    recipe app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
+import React, {useState} from 'react';
 import './App.css';
+import Axios from 'axios';
+import RecipeItem from './components/RecipeItem';
+
+
 
 function App() {
+
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  
+
+  const APP_ID = "c25da38d";
+  const APP_KEY = "bb169650d54eb21815483fa61c540a50";
+
+  const url=`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  async function getRecipes(){
+    var result = await Axios.get(url);
+    setRecipes(result.data.hits);
+    console.log(result.data.hits);
+  }
+  
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipes();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <h1>Food Recipe 🍔🍩</h1>
+      <form className="app__searchForm" onSubmit={onSubmit}>
+        <input type="text" placeholder="Search for a Recipe...." className="app__input"
+        value={query} onChange={(e) => setQuery(e.target.value)}/>
+        <input className="app__submit" type="submit" value="Search"/>
+      
+
+      
+      </form>
+      <div className="app_recipes">
+      { 
+        recipes !== []  &&
+        recipes.map((recipe) => {
+          return <RecipeItem recipe={recipe}/>;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/RecipeItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './RecipeItem.css';
+import {v4 as uuidv4} from 'uuid';
+
+export default function RecipeItem({recipe}) {
+  return (
+        recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
+    <div className="recipeItem" onClick={()=>window.open(recipe["recipe"]["image"])}>
+    <img className="recipeItem__img" src={recipe["recipe"]["image"]} alt="recipe"/>
+    <p className="recipeItem__name" key={uuidv4()}>
+        {recipe["recipe"]["label"]}
+    </p>
+    </div>
+        )
+  );
+}
+
+
