88d9a95279ab4a8897631ebe570bbbbcd3a80e2d
Kartik Gupta <gupta.kartik18kg@gmail.com>
2022-07-25 22:02:13 +0530

    No need to use a match
    .match function on a string returns an array of matches or null, in this case for all images it'll return null because the image format doesn't match with png,jpg,gif or png.

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import './RecipeItem.css';
-import {v4 as uuidv4} from 'uuid';
+import React from "react";
+import "./RecipeItem.css";
+import { v4 as uuidv4 } from "uuid";
 
-export default function RecipeItem({recipe}) {
+export default function RecipeItem({ recipe }) {
+  console.log(recipe["recipe"]["image"])
   return (
-        recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
-    <div className="recipeItem" onClick={()=>window.open(recipe["recipe"]["image"])}>
-    <img className="recipeItem__img" src={recipe["recipe"]["image"]} alt="recipe"/>
-    <p className="recipeItem__name" key={uuidv4()}>
-        {recipe["recipe"]["label"]}
-    </p>
-    </div>
-        )
+    recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
+      <div
+        className="recipeItem"
+        onClick={() => window.open(recipe["recipe"]["image"])}
+      >
+        <img
+          className="recipeItem__img"
+          src={recipe["recipe"]["image"]}
+          alt="recipe"
+        />
+        <p className="recipeItem__name" key={uuidv4()}>
+          {recipe["recipe"]["label"]}
+        </p>
+      </div>
+    )
   );
 }
-
-
